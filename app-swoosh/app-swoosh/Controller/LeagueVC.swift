725078a7c0df87a42de140386474ba1806e0e690
Bartłomiej Moniak <bm@Bartomiejs-MacBook-Pro.local>
2017-08-13 23:46:05 +0200

    finished for now

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,27 +10,42 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
     
+    @IBOutlet weak var nextBtn: BorderButton!
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCsegue", sender: self)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func selectLeague(leagueType: String) {
+        player.selectedLeague = leagueType
+        nextBtn.isEnabled = true
     }
-    */
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+ 
 
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Bartłomiej Moniak on 13/08/2017.
+//  Copyright © 2017 Bartłomiej Moniak. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var selectedLeague: String!
+    var selectedSkillLevel: String!
+}
